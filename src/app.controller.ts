023eb7c1480a18f720f1fecf2948ed947fbeca9f
Vikram Chowdhury <vikram@themetromaxgroup.com>
2023-09-08 17:03:43 +0530

    moved business logic in service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,18 @@
 import { Controller, Param, Body, Get, Delete, Patch, Post} from "@nestjs/common";
 import { data, ReportType } from "./data";
-import { v4 as uuid } from "uuid";
+import { AppService } from "./app.service";
 
 @Controller("report/:type/")
 export class AppController {
 
+  constructor(
+    private readonly appService: AppService
+  ){}
+
   @Get()
   getAllREports(@Param('type') type: string) {
     const reportType = type === ReportType.INCOME ? ReportType.INCOME : ReportType.EXPENSE
-    return data.report.filter((report) => {
-      return report.type === reportType;
-    });
+    return this.appService.getAllReportsService(reportType)
   }
 
   @Get(":id")
@@ -19,10 +21,8 @@ export class AppController {
     @Param('id') id: string
   ) {
     const reportType = type === ReportType.INCOME ? ReportType.INCOME : ReportType.EXPENSE;
-    console.log(id, type, reportType)
-    return data.report
-    .filter((report) => report.type === reportType)
-    .find((report) => report.id === id)
+    return this.appService.getReportByIdService(reportType, id)
+
   }
 
   @Post()
@@ -31,17 +31,8 @@ export class AppController {
     @Body() {amount, source}: { amount: number;source: string }
   ) {
     const reportType = type === ReportType.INCOME ? ReportType.INCOME : ReportType.EXPENSE;
+    this.appService.createReportService(reportType,amount, source)
 
-    const newReport = {
-      id: uuid(),
-      source: source,
-      amount: amount,
-      created_at: new Date(),
-      updated_at: new Date(),
-      type: reportType
-    }
-    data.report.push(newReport)
-    return reportType
   }
   @Patch(":id")
   updateReportById(
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { ReportType, data } from "./data";
+import { Injectable } from "@nestjs/common/decorators";
+import { v4 as uuid } from "uuid";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  getAllReportsService(type: ReportType) {
+    return data.report.filter((report) => {
+      return report.type === type;
+    });
   }
+
+  getReportByIdService(type: ReportType, id: string) {
+    return data.report
+    .filter((report) => report.type === type)
+    .find((report) => report.id === id)
+  }
+
+  createReportService(type: ReportType, amount: number, source: string) {
+    const newReport = {
+      id: uuid(),
+      source: source,
+      amount: amount,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: type
+    }
+    data.report.push(newReport)
+    return "created" 
+  }
+
 }
