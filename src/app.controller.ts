3146d29c1b150a7b9e175cb4432152aa864f398f
Vikram Chowdhury <vikram@themetromaxgroup.com>
2023-09-08 14:30:20 +0530

    Added Crud Functions

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Delete, Patch, Post} from "@nestjs/common";
 
-@Controller()
+@Controller("report/:type/")
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAllREports() {
+    return {"reports":1};
+  }
+
+  @Get(":id")
+  getReportById() {
+    return {"reportById": 'id'}
+  }
+
+  @Post()
+  createReport() {
+    return {"Created":1}
+  }
+  @Patch(":id")
+  updateReportById() {
+    return {"Updated Report": 1}
+  }
+  @Delete(":id")
+  deleteReportById() {
+    return {"Deleted Report":1}
   }
 }
